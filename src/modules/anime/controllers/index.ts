801a1fa305df6cf9b378c1e0fee5930b91b5b527
Worst One <luccagabriel12@hotmail.com>
2024-04-08 17:57:41 -0300

    .

--- src/modules/anime/controllers/index.ts
+++ src/modules/anime/controllers/index.ts
@@ -7,18 +7,22 @@ import { challenge } from "@src/utils/o_auth_2_utils";
 const animeController = new Elysia().use(authService);
 
 animeController
-  .get("/anime/redirect_mal", async ({ headers, query, body, error }) => {
-    try {
-      console.log("/anime/redirect_mal - body", body);
-      console.log("/anime/redirect_mal - headers", headers);
-      console.log("/anime/redirect_mal - query", query);
+  .get(
+    "/anime/redirect_mal/:_id",
+    async ({ headers, query, body, params, error }) => {
+      try {
+        console.log("params._id", params._id);
+        console.log("/anime/redirect_mal - body", body);
+        console.log("/anime/redirect_mal - headers", headers);
+        console.log("/anime/redirect_mal - query", query);
 
-      return { status: 200, payload: "Conta Conectada com Sucesso", msg: "" };
-    } catch (exception) {
-      console.log("Controller User :: getAll", exception);
-      return error(500, { exception, msg: "" });
+        return { status: 200, payload: "Conta Conectada com Sucesso", msg: "" };
+      } catch (exception) {
+        console.log("Controller User :: getAll", exception);
+        return error(500, { exception, msg: "" });
+      }
     }
-  })
+  )
   .guard(
     {
       verifyToken: true,
@@ -26,40 +30,26 @@ animeController
     (appGuard) =>
       appGuard.group("/anime", (app) =>
         //
-        app
-          .get("/authenticate_mal", async ({ error }) => {
-            try {
-              const response = await fetch(
-                `https://myanimelist.net/v1/oauth2/authorize?
+        app.get("/link_mal_account", async ({ authUser, error }) => {
+          try {
+            const redirectUrl = `https://myanimelist.net/v1/oauth2/authorize?
               response_type=code
               &client_id=${process.env.MAL_CLIENT_ID}
-              &state=YOUR_STATE
-              &redirect_uri=http://localhost:4000/anime/redirect_mal
+              &state=${authUser._id}
+              &redirect_uri=http://localhost:4000/anime/redirect_mal/${authUser._id}
               &code_challenge=${challenge}
-              &code_challenge_method=plain `,
-                {
-                  method: "GET",
-                  headers: { "Content-Type": "application/x-www-form-urlencoded" },
-                }
-              );
+              &code_challenge_method=plain`;
+            // {
+            //   method: "GET",
+            //   headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            // }
 
-              const body = await response.json();
-              console.log(body);
-
-              return { status: 200, payload: "", msg: "" };
-            } catch (exception) {
-              console.log("Controller User :: getAll", exception);
-              return error(500, { exception, msg: "" });
-            }
-          })
-          .post("/link_mal", async ({ body, error }) => {
-            try {
-              return { status: 200, payload: "", msg: "" };
-            } catch (exception) {
-              console.log("Controller User :: getAll", exception);
-              return error(500, { exception, msg: "" });
-            }
-          })
+            return { status: 200, payload: redirectUrl, msg: "" };
+          } catch (exception) {
+            console.log("Controller User :: getAll", exception);
+            return error(500, { exception, msg: "" });
+          }
+        })
       )
   );
 
